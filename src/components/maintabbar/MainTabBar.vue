f82d1fd870f21c3a04ce1f51b2a091024db6d0c1
coded <dingkaiqiang@outlook.com>
2020-02-14 21:46:40 +0800

    first test

---
+++ src/components/maintabbar/MainTabBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <tab-bar>
+      <tab-bar-item path="/home" activeColor="deeppink">
+        <img slot="item-icon" src="~assets/img/home.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/home_active.svg" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor="deeppink">
+        <img slot="item-icon" src="~assets/img/category.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/category_active.svg" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" activeColor="deeppink">
+        <img slot="item-icon" src="~assets/img/shopcart.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/shopcart_active.svg" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile" activeColor="deeppink">
+        <img slot="item-icon" src="~assets/img/profile.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/profile_active.svg" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>      
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from 'components/tabbar/TabBar'
+import TabBarItem from 'components/tabbar/TabBarItem'
+
+  export default {
+    components: {
+      TabBar,
+      TabBarItem
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/tabbar/TabBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+  #tab-bar {
+    display: flex;
+    background-color: #f6f6f6;  
+
+    position: fixed;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+
+    box-shadow: 0px -1px 2px rgba(150, 150, 150, 0.8)
+  }
+
+</style>
